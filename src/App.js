c3c621df272ec9367d24e7c05b5b358b0a4af070
Kevalin Ketcham <kevalin.create@gmail.com>
2020-11-10 20:10:31 -0800

    Build basic page structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import plusElement from './plusElement.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <nav className="nav">
+          <div>
+            <img src={logo} alt="logo" />
+            <h4>Atomic Search</h4>
+          </div>
+          <div>
+            <a href="">Twitter</a>
+            <a href="">Contact</a>
+          </div>
+          <hr/>
+        </nav>
+        <div className="body">
+          <div className="plusElement">
+            <img id="plusElement" src={plusElement}></img>
+          </div>
+          <div className="right elements">
+            <div id="us">
+              <h2>Who we are</h2>
+
+            </div>
+            <div id="join">
+              <h2>Join us</h2>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bottomBar"></div>
+    </>
   );
 }
 
